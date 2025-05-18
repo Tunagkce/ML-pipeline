641635dd21fef6aadf31b71e14fcaa821b7b154e
Tunagkce <159780183+Tunagkce@users.noreply.github.com>
2025-05-19 00:56:53 +0300

    Update deploy.py

--- Project/deploy.py
+++ Project/deploy.py
@@ -10,4 +10,4 @@ MODEL_PATH = os.path.join(BASE_DIR, 'bank_marketing.sav')
 # Load the model safely
 with open(MODEL_PATH, 'rb') as f:
     model = pickle.load(f)
-
+st.set_page_config(page_title="Deposit Prediction App", layout="wide", page_icon="🏦")
