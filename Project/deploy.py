b808f71d4f3f9b77582940a81346c01d337ec163
Tunagkce <159780183+Tunagkce@users.noreply.github.com>
2025-05-19 00:50:18 +0300

    Update deploy.py

--- Project/deploy.py
+++ Project/deploy.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import os
 import pickle
-import scikit-learn
+import sklearn
 # Get absolute path to current script
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(BASE_DIR, 'bank_marketing.sav')
