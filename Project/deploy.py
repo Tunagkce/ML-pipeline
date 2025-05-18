cf501f20ce9057c5ba9d9fe07f9c3de45746141a
Tunagkce <159780183+Tunagkce@users.noreply.github.com>
2025-05-19 00:42:35 +0300

    Update deploy.py

--- Project/deploy.py
+++ Project/deploy.py
@@ -3,15 +3,5 @@ import pickle
 import pandas as pd
 
 st.set_page_config(page_title="Deposit Prediction App", layout="wide", page_icon="🏦")
-model=pickle.load(open('Project/bank_marketing.sav','rb'))
-# this is the main function in which we define our webpage  
-def main():
-    # front end elements of the web page 
-    html_temp = """ 
-    <div style ="background-color:yellow;padding:13px"> 
-    <h1 style ="color:black;text-align:center;">Bank Marketing Prediction App</h1> 
-    </div> 
-    """
-      
-    # display the front end aspect
-    st.markdown(html_temp, unsafe_allow_html = True) 
+model=pickle.load(open('bank_marketing.sav','rb'))
+
