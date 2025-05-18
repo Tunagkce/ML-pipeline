a0fd09d43913352d27dbc604c513fa5d2c5e180a
Tunagkce <159780183+Tunagkce@users.noreply.github.com>
2025-05-19 01:57:33 +0300

    Update deploy.py

--- Project/deploy.py
+++ Project/deploy.py
@@ -52,6 +52,10 @@ def main():
     cons_conf_idx = st.number_input("Consumer Confidence Index")
     euribor3m = st.number_input("Euribor 3 Month Rate")
     nr_employed = st.number_input("Number of Employees")
+
+    # was_contacted_before derived from pdays
+    was_contacted_before_input = st.selectbox("Was Contacted Before?", ['yes', 'no'])
+    was_contacted_before = 1 if was_contacted_before_input == 'yes' else 0
    
 if __name__ == '__main__':
     main()
